47acf78de8ff9d375eecda409d4593c47e06fbb9
Aman <amanjain7838@gmail.com>
2020-02-23 14:32:35 +0530

    first v

--- index.js
+++ index.js
@@ -1,36 +1,123 @@
-let app = require('express')();
+const express = require('express');
+const app = express();
 let http = require('http').Server(app);
+// const  bodyParser  =  require('body-parser');
+const cors = require('cors')
+// const  sqlite3  =  require('sqlite3').verbose();
+const  jwt  =  require('jsonwebtoken');
+const  bcrypt  =  require('bcryptjs');
+const SECRET_KEY = "secretkey23456";
+
+// let io = require('socket.io')(http);
+// const friendsRouter = require('./routes/friends.js');  
+// let server = app.listen(3000);
 let io = require('socket.io')(http);
- 
+
+let userRouter = require('./routes/user');  
+let socketRouter = require('./routes/socket');  
+
+
+// const projectRouter = require('./routes/project.js');  
+
+// app.set("io", io);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// app.all('/*', function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
+// app.get('/user', function (req, res) {
+//   res.send('GET request to the homepage')
+// })
+app.use('/user', userRouter);
+app.use('/socket', socketRouter);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status=404;
+  next(error);
+})
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error:{
+      message: error.message
+    }
+  })
+})
 var rooms = [];
+var currectactiveuser=[];
+// app.use('/project', projectRouter);
 
 io.on('connection', (socket) => {
-  
+  let currentRoom = ("" + Math.random()).substring(2, 7);
+  socket.join(currentRoom);
+
+  socket.on('chatdetails',(data)=>{
+      var new_room = ("" + Math.random()).substring(2, 7);
+      rooms.push(new_room);
+      data.receiverUser,join(new_room);
+      data.currentUser,join(new_room);
+      socket.emit('roomcreated',new_room);
+  });
+
   socket.on('disconnect', function(){
     socket.leave(socket.room);
     io.emit('users-changed', {user: socket.username, event: 'left'});   
   });
  
-  socket.on('set-name', (name) => {
-    socket.username = name;
-    io.emit('users-changed', {user: name, event: 'joined'});    
-  });
-  
-  socket.on('send-message', (message) => {
-    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});    
+  socket.on('setroom', (data) => {
+    let roomfound=0;    
+    var new_room = ("" + Math.random()).substring(2, 7);
+    for(var i=0;i<currectactiveuser.length;i++)
+    {
+      if((currectactiveuser[i]['friendid']==data.userid&&currectactiveuser[i]['userid']==data.friendid)||(currectactiveuser[i]['userid']==data.friendid&&currectactiveuser[i]['friendid']==data.userid)||(currectactiveuser[i]['friendid']==data.friendid&&currectactiveuser[i]['userid']==data.userid))
+      {
+        roomfound=1;
+        new_room=currectactiveuser[i]['roomid'];
+        break;
+      }
+    }
+    socket.leave(currentRoom);
+    socket.join(new_room);
+    let chatdetails={};
+    if(!roomfound)
+    {
+      chatdetails['friendid']=data.friendid;
+      chatdetails['userid']=data.userid;
+      chatdetails['roomid']=new_room;
+      currectactiveuser.push(chatdetails);
+    }
+    console.log(currectactiveuser)
+    socket.emit('listenroomid', {
+      sender: '***SERVER***',
+      content: new_room
+    })
   });
 
-  socket.on('add-message', (message) => {
-    io.emit('message', {text: message.text, user: socket.username, created: new Date()});    
+  socket.on('send-message', (message) => {
+    let data={msg: message.text,sender:message.sender,receiver:message.receiver, createdAt: new Date()};
+    io.to(message.roomid).emit('message',data );    
   });
+
+  // socket.on('add-message', (message) => {
+  //   io.emit('message', {text: message.text, user: socket.username, created: new Date()});    
+  // });
 
 
   socket.on('createroom', function (data) {
       var new_room = ("" + Math.random()).substring(2, 7);
       rooms.push(new_room);
-      data.room = new_room;
-      socket.emit('updatechat', 'SERVER', 'Your room is ready, invite someone using this ID:' + new_room);
-      socket.emit('roomcreated', data);
+      socket.emit('roomcreated',new_room);
+      // data.room = new_room;
+      // socket.emit('updatechat', 'SERVER', 'Your room is ready, invite someone using this ID:' + new_room);
   });
 
 
