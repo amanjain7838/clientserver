37f1d3f05445ab63a75391261e2357f926f9dcf1
Aman <amanjain7838@gmail.com>
2020-03-10 01:42:57 +0530

    chat design

--- index.js
+++ index.js
@@ -41,69 +41,57 @@ var rooms = [];
 var currectactiveuser=[];
 
 io.on('connection', (socket) => {
-  socket.on('disconnect', function(){
-    socket.leave(socket.room);
-    io.emit('users-changed', {user: socket.username, event: 'left'});   
-  });
+  	socket.on('disconnect', function(){
+    	socket.leave(socket.room);
+    	io.emit('users-changed', {user: socket.username, event: 'left'});   
+  	});
  
-  socket.on('setroom', (data) => {
-    let roomfound=0;    
-    var new_room = ("" + Math.random()).substring(2, 7);
-    for(var i=0;i<currectactiveuser.length;i++)
-    {
-      if((currectactiveuser[i]['friendid']==data.userid&&currectactiveuser[i]['userid']==data.friendid)||(currectactiveuser[i]['userid']==data.friendid&&currectactiveuser[i]['friendid']==data.userid)||(currectactiveuser[i]['friendid']==data.friendid&&currectactiveuser[i]['userid']==data.userid))
-      {
-        roomfound=1;
-        new_room=currectactiveuser[i]['roomid'];
-        break;
-      }
-    }
-    // socket.leave(currentRoom);
-    socket.join(new_room);
-    let chatdetails={};
-    if(!roomfound)
-    {
-      chatdetails['friendid']=data.friendid;
-      chatdetails['userid']=data.userid;
-      chatdetails['roomid']=new_room;
-      currectactiveuser.push(chatdetails);
-    }
-    console.log(currectactiveuser)
-    socket.emit('listenroomid', {
-      sender: '***SERVER***',
-      content: new_room
-    })
-  });
+	socket.on('setroom', (data) => {
+		    let roomfound=0;    
+		    var new_room = ("" + Math.random()).substring(2, 7);
+		    for(var i=0;i<currectactiveuser.length;i++)
+		    {
+		      if((currectactiveuser[i]['friendid']==data.userid&&currectactiveuser[i]['userid']==data.friendid)||(currectactiveuser[i]['userid']==data.friendid&&currectactiveuser[i]['friendid']==data.userid)||(currectactiveuser[i]['friendid']==data.friendid&&currectactiveuser[i]['userid']==data.userid))
+		      {
+		        roomfound=1;
+		        new_room=currectactiveuser[i]['roomid'];
+		        break;
+		      }
+		    }
+		    getSocketlogs(data).then(function(chatdata){
+		    	socket.emit('chathistory', chatdata);	
+		    });
+		    // socket.leave(currentRoom);
+		    socket.join(new_room);
+		    let chatdetails={};
+		    if(!roomfound)
+		    {
+		      chatdetails['friendid']=data.friendid;
+		      chatdetails['userid']=data.userid;
+		      chatdetails['roomid']=new_room;
+		      currectactiveuser.push(chatdetails);
+		    }
+		    console.log(currectactiveuser)
+		    socket.emit('listenroomid', {
+		      content: new_room
+		    })
 
-  socket.on('send-message', (message) => {
-    let data={msg: message.text,sender:message.sender,receiver:message.receiver, createdAt: new Date()};
-    io.to(message.roomid).emit('message',data );
-	
-	var clients = socket.id;
-	// var clients = io.sockets.clients('room'); 
-	console.log(clients)
+	});
 
-    socketlogs(message);
-  });
-
-
-  socket.on('createroom', function (data) {
-      var new_room = ("" + Math.random()).substring(2, 7);
-      rooms.push(new_room);
-      socket.emit('roomcreated',new_room);
-      // data.room = new_room;
-      // socket.emit('updatechat', 'SERVER', 'Your room is ready, invite someone using this ID:' + new_room);
-  });
-
-
-  // socket.on('sendchat', function (data) {
-  //     io.sockets.in(socket.room).emit('updatechat', socket.username, data);
-  // });
+  	socket.on('send-message', (message) => {
+	    let data={message: message.text,senderId:message.sender,receiverId:message.receiver, createdAt: new Date()};
+	    io.to(message.roomid).emit('message',data );
+	    setSocketlogs(message);
+  	});
 
 });
-function socketlogs(message)
+function setSocketlogs(message)
 {
   savesocketlogs(message);
+}
+function getSocketlogs(data)
+{
+	return retrieveSocketlogs(data);
 }
  
 var port = process.env.PORT || 3001;
--- routes/socketthread.js
+++ routes/socketthread.js
@@ -1,4 +1,6 @@
 var models  = require('../models');
+const { Op } = require("sequelize");
+
 module.exports = function() { 
   this.savesocketlogs=function(message){
       createthread(message);
@@ -19,11 +21,33 @@ module.exports = function() {
       //     console.log(data)
       // });
   }
+  this.retrieveSocketlogs=function(data){
+    return retrieveSocketlogs(data);
+  }
   const createthread=(message)=>{
     models.thread.create({
         senderId:message['sender'],
         receiverId:message['receiver'],
         message:message['text'],
+    });
+  }
+  const retrieveSocketlogs=(data)=>{
+    return models.thread.findAll({
+        where: {
+            [Op.or]:[
+              {
+                receiverId:data['userid'],
+                senderId:data['friendid']
+              },
+              {
+                senderId:data['userid'],
+                receiverId:data['friendid']
+              }
+            ],
+        },
+        order: [ [ 'createdAt', 'ASC' ]],
+    }).then(function(result) {
+        return result;
     });
   }
 }
