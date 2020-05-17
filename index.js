c761fb35156e06d399d404e0784377ce0aaf0f3a
Aman <amanjain7838@gmail.com>
2020-05-17 15:25:00 +0530

    sprint

--- index.js
+++ index.js
@@ -43,47 +43,20 @@ var currectactiveuser=[];
 io.on('connection', (socket) => {
   	socket.on('disconnect', function(){
     	socket.leave(socket.room);
-    	io.emit('users-changed', {user: socket.username, event: 'left'});   
   	});
- 
 	socket.on('setroom', (data) => {
-		    let roomfound=0;    
-		    var new_room = ("" + Math.random()).substring(2, 7);
-		    for(var i=0;i<currectactiveuser.length;i++)
-		    {
-		      if((currectactiveuser[i]['friendid']==data.userid&&currectactiveuser[i]['userid']==data.friendid)||(currectactiveuser[i]['userid']==data.friendid&&currectactiveuser[i]['friendid']==data.userid)||(currectactiveuser[i]['friendid']==data.friendid&&currectactiveuser[i]['userid']==data.userid))
-		      {
-		        roomfound=1;
-		        new_room=currectactiveuser[i]['roomid'];
-		        break;
-		      }
-		    }
-		    getSocketlogs(data).then(function(chatdata){
-		    	socket.emit('chathistory', chatdata);	
-		    });
-		    // socket.leave(currentRoom);
-		    socket.join(new_room);
-		    let chatdetails={};
-		    if(!roomfound)
-		    {
-		      chatdetails['friendid']=data.friendid;
-		      chatdetails['userid']=data.userid;
-		      chatdetails['roomid']=new_room;
-		      currectactiveuser.push(chatdetails);
-		    }
-		    console.log(currectactiveuser)
-		    socket.emit('listenroomid', {
-		      content: new_room
-		    })
-
+		    socket.join(data.roomowner);
 	});
-
+	socket.on('get-chathistory', (params) => {
+	    getSocketlogs(params).then(function(chatdata){
+	    	io.to(params.userid).emit('chathistory', chatdata);	
+	    });
+  	});
   	socket.on('send-message', (message) => {
 	    let data={message: message.text,senderId:message.sender,receiverId:message.receiver, createdAt: new Date()};
-	    io.to(message.roomid).emit('message',data );
+	    io.to(message.receiver).emit('message',data );
 	    setSocketlogs(message);
   	});
-
 });
 function setSocketlogs(message)
 {
