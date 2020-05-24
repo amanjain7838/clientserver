27c0fdb89cb1ca86db8374315281327166f5b03f
Aman <amanjain7838@gmail.com>
2020-05-24 14:45:23 +0530

    chat final

---
+++ migrations/20200523103959-chatstructure.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return Promise.all([
+      queryInterface.addColumn(
+        'users',
+        'profilePicture',
+        {
+          type:Sequelize.STRING(20),
+          allowNull: true
+        }
+      ),
+      queryInterface.addColumn(
+        'threads',
+        'lastSeen',
+        {
+          type:'TIMESTAMP',
+          allowNull:true,
+        }
+      ),
+      queryInterface.addColumn(
+        'threads',
+        'userRelation',
+        {
+          type:Sequelize.INTEGER,
+          allowNull:false,
+          // references: { model: 'userfriends', key: 'id' }
+        }
+      ),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeColumn('users', 'profilePicture');
+
+  }
+};
+
+
